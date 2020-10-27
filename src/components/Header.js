cf82b3b0f459d1226ec10d440a56ef9bc66a716f
Alison Carnes <alisoncarnes@alisons-air.lan>
2020-10-27 14:00:27 -0700

    pushing files

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header(props) {
     return (
       <header>
-        <h1>you had me<br/> at beer</h1>
+        <h1>you had me<br/> at beer.</h1>
       </header>
     )
 }
